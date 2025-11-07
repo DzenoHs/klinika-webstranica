fb6676abebd8d105fa7076e5c0de0278033a347e
Dzenan Hasandic <dzenan.dzeno2017@gmail.com>
2025-11-07 14:59:07 +0100

    Update

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -61,6 +61,10 @@ const SEO: React.FC<SEOProps> = ({
       <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
       <meta name="googlebot" content="index, follow" />
       
+      {/* Remove old cached URLs */}
+      <meta name="google" content="nositelinksearchbox" />
+      <meta name="google" content="notranslate" />
+      
       {/* Additional SEO tags */}
       <meta name="rating" content="general" />
       <meta name="distribution" content="global" />
